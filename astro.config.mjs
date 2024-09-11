47c1a098e19094d37d9a74fd5ea62f5ca433c8df
edwin6666 <edwinmendoza0620@gmail.com>
2024-09-11 16:44:33 -0300

    Update astro.config.mjs to include robots.txt integration and sitemap URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://edwindev6-portfolio.vercel.app/'
+  integrations: [
+    tailwind(),
+    robotsTxt({
+      policy: [{ userAgent: '*', allow: '/' }],
+      sitemap: 'https://edwindev6-portfolio.vercel.app/sitemap.xml',
+    })
+  ],
+  site: 'https://edwindev6-portfolio.vercel.app'
 })
